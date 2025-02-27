f2006854c6ce6277e105c5f458bdd6ec167cfba8
bracossj <luisfelipece02@gmail.com>
2025-02-26 22:18:25 -0500

    commit_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [newTaskName, setNewTaskName] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('tasks', newTaskName);
+    setNewTaskName('');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app'>
+        <h1>Gestor de Tareas</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='Ingrese nueva tarea' value={newTaskName} onChange={(e) => setNewTaskName(e.target.value)} />
+
+          <button> Agregar</button>
+        </form>
       </div>
-      <h1>Gestor Creado por Luis Caro</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
